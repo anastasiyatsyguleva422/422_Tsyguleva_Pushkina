82af3c81846e21c723d9cb8a057a58eb0c2ab5fa
User <User@DESKTOP-R23HS44>
2025-10-23 12:00:52 +0300

    Добавьте файлы проекта.

---
+++ 422_Tsyguleva_Pushkina/DbContextHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _422_Tsyguleva_Pushkina
+{
+    public static class DbContextHelper
+    {
+        public static Tsyguleva_Pushkina_DB_PaymentEntities GetContext()
+        {
+            return new Tsyguleva_Pushkina_DB_PaymentEntities();
+        }
+
+        public static List<T> GetAll<T>() where T : class
+        {
+            using (var context = new Tsyguleva_Pushkina_DB_PaymentEntities())
+            {
+                return context.Set<T>().ToList();
+            }
+        }
+
+        //public static void RemoveRange<T>(List<T> entities) where T : class
+        //{
+        //    using (var context = new Tsyguleva_Pushkina_DB_PaymentEntities())
+        //    {
+        //        context.Set<T>().RemoveRange(entities);
+        //        context.SaveChanges();
+        //    }
+        //}
+    }
+}
---
+++ 422_Tsyguleva_Pushkina/Pages/UsersTabPage.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _422_Tsyguleva_Pushkina.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для UsersTabPage.xaml
+    /// </summary>
+
+    public partial class UsersTabPage : Page
+    {
+
+            public UsersTabPage()
+            {
+                InitializeComponent();
+
+                DataGridUser.ItemsSource = DbContextHelper.GetContext().User.ToList();
+                this.IsVisibleChanged += Page_IsVisibleChanged;
+            }
+
+            private void UpdateUsersData()
+            {
+                DataGridUser.ItemsSource = DbContextHelper.GetContext().User.ToList();
+                DataGridUser.Items.Refresh(); 
+            }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Visibility == Visibility.Visible)
+            {
+                DbContextHelper.GetContext().ChangeTracker.Entries().ToList().ForEach(x => x.Reload());
+                DataGridUser.ItemsSource = DbContextHelper.GetContext().User.ToList();
+            }
+        }
+        private void ButtonAdd_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService?.Navigate(new AddUserPage(null));
+        }
+        private void ButtonDel_Click(object sender, RoutedEventArgs e)
+        {
+            var usersForRemoving = DataGridUser.SelectedItems.Cast<User>().ToList();
+            if (MessageBox.Show($"Вы точно хотите удалить записи в количестве {usersForRemoving.Count()} элементов?", "Внимание", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                try
+                {
+
+                    DbContextHelper.GetContext().User.RemoveRange(usersForRemoving);
+                    DbContextHelper.GetContext().SaveChanges();
+                    MessageBox.Show("Данные успешно удалены!");
+                    DataGridUser.ItemsSource = DbContextHelper.GetContext().User.ToList();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+        }
+        private void ButtonEdit_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Pages.AddUserPage((sender as Button).DataContext as User));
+        }
+    }
+} 
+
+ 
